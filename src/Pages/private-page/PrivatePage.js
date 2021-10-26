6df495084400397cc4a9861c19af294e373a7729
elitandilashvili <elielitandilashvili@gmail.com>
2021-10-26 23:38:29 +0400

    add

--- src/Pages/private-page/PrivatePage.js
+++ src/Pages/private-page/PrivatePage.js
@@ -1,10 +1,7 @@
-
-
-export const PrivatePage =({title=" Private page"})=>{
-    return(
-        <div className="column">
-            <h3 className="title"> {title} </h3>
-
-        </div>
-    );
+export const PrivatePage = ({ title = ' Private page' }) => {
+  return (
+    <div className="column">
+      <h3 className="title"> {title} </h3>
+    </div>
+  );
 };
